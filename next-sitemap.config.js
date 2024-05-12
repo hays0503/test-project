07fb10931b3c157fd835ca62d74a2411b78a8ddc
hays0503 <cthtufgbv@mail.ru>
2024-05-12 19:52:47 +0500

    добавил sitemap и robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://pimenov.kz',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { Metadata } from 'next';
+ 
+//Метаданные для страницы
+export const metadata: Metadata = {
+  title: "Сайт для тестов",
+  description: "Сайт для тестов на VDS hoster.kz",
+  keywords: "nextjs, react, typescript, css-modules, vds, hoster.kz, letsencryptm, pimenov.kz, sck-1.kz, Пименов Сергей"
+};
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
       <div className={`${styles.description} ${styles.center}`}>
